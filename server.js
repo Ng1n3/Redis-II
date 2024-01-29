d0f3affe12a0c3fc012ba72bbe6defb7a8e5664b
ng1n3 <code.with.muyiwa@gmail.com>
2024-01-29 22:20:28 +0100

    fixed up other cases around redis and refactor the code

--- server.js
+++ server.js
@@ -15,29 +15,40 @@ app.use(cors());
 
 app.get("/photos", async (req, res) => {
   const albumId = req.query.albumId;
-  client.get("photos", async (err, photos) => {
-    if (err) console.log(err);
-    if (photos !== null) {
-      console.log("cache hit");
-      return res.json(JSON.parse(photos));
-    }
-    console.log('cache miss');
+  const photos = await getOrSetCache(`photos?albumId=${albumId}`, async () => {
     const { data } = await axios.get(
       "https://jsonplaceholder.typicode.com/photos",
       {
         params: { albumId },
       }
     );
-    client.setEx("photos", DEFAULT_EXPIRATION, JSON.stringify(data));
-    res.json(data);
+    return data;
   });
+  res.json(photos);
 });
 
-app.get("photos/:id", async (req, res) => {
-  const { data } = await axios.get(
-    `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
-  );
-  res.json(data);
+app.get("/photos/:id", async (req, res) => {
+  console.log('Hit id');
+  const photo = await getOrSetCache(`photos:${req.params.id}`, async () => {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+    );
+    return data;
+  });
+  res.json(photo);
 });
+
+function getOrSetCache(key, cb) {
+  return new Promise((resolve, reject) => {
+    client.get(key, async (err, data) => {
+      if(err) return reject(err)
+      if(data !== null) return resolve(JSON.parse(data))
+      const freshData = await cb()
+      client.setEx(key, DEFAULT_EXPIRATION, JSON.stringify(freshData))
+      resolve(freshData)
+    })
+  })
+}
+
 
 app.listen(4000);
