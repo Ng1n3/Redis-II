84c4bfc714854d429e64a4891dfb37fc4e30d0d2
ng1n3 <code.with.muyiwa@gmail.com>
2024-01-29 21:52:51 +0100

    web caching implemented

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const redis = require("redis");
+
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const client = redis.createClient({ legacyMode: true, PORT: REDIS_PORT });
+
+client.connect().catch((err) => console.error(err));
+const DEFAULT_EXPIRATION = 3600;
 
 const app = express();
 app.use(cors());
 
 app.get("/photos", async (req, res) => {
   const albumId = req.query.albumId;
-  const { data } = await axios.get("https://placeholder.typicode.com/photos", {
-    params: { albumId },
+  client.get("photos", async (err, photos) => {
+    if (err) console.log(err);
+    if (photos !== null) return res.json(JSON.parse(photos));
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/photos",
+      {
+        params: { albumId },
+      }
+    );
+    client.setEx("photos", DEFAULT_EXPIRATION, JSON.stringify(data));
+    res.json(data);
   });
-  res.json(data);
 });
 
 app.get("photos/:id", async (req, res) => {
@@ -20,4 +36,4 @@ app.get("photos/:id", async (req, res) => {
   res.json(data);
 });
 
-app.listen(4000)
+app.listen(4000);
